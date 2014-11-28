8c74bc5c698b23c3395d336dbbe1a0b946c1d57b
teleaziz <abdz.abbas@gmail.com>
2014-11-28 13:48:34 -0800

    first commit

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('chatAppApp')
+.controller('MainCtrl', function (Auth,$scope, $http, socket, $window) {
+  $scope.awesomeThings = [];
+
+  $http.get('/api/things').success(function(awesomeThings) {
+    $scope.awesomeThings = awesomeThings;
+    socket.syncUpdates('thing', $scope.awesomeThings);
+  });
+
+  $scope.addThing = function() {
+    if($scope.newThing === '') {
+      return;
+    }
+    $http.post('/api/things', { name: $scope.newThing });
+    $scope.newThing = '';
+  };
+
+  $scope.deleteThing = function(thing) {
+    $http.delete('/api/things/' + thing._id);
+  };
+
+  $scope.$on('$destroy', function () {
+    socket.unsyncUpdates('thing');
+  });
+
+  $scope.loginOauth = function(provider) {
+    $window.location.href = '/auth/' + provider;
+  };
+  $scope.isLoggedIn = Auth.isLoggedIn;
+});
