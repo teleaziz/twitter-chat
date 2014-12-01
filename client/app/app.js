585680f874194f6f6060cd81625355f02cc80cea
teleaziz <abdz.abbas@gmail.com>
2014-11-30 19:34:00 -0800

    simple animation on transition of the page

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('chatAppApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngAnimate'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
